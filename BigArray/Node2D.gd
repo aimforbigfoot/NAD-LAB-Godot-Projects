303910ad67f2e7cea38fc7c4b89fa22bf2fe37fe
NAD LAB <61890796+aimforbigfoot@users.noreply.github.com>
2020-07-22 10:23:18 -0700

    Updated for a big test
    Automation m8

--- BigArray/Node2D.gd
+++ BigArray/Node2D.gd
@@ -2,20 +2,28 @@ extends Node2D
 
 var file := File.new()
 var path := "user://arrtemp.dat"
-var thicc_arr := []
+var thicc_arr :PoolIntArray= []
 var do_the_array := false
 
 func _ready() -> void:
-	if do_the_array:
-		randomize()
-		for i in 100000:
-	#		print(i)
-			thicc_arr.append(1)
-	#		thicc_arr.append(10)
-	#		thicc_arr.append(randi())
-		file.open(path, File.WRITE)
-		file.store_var(thicc_arr)
-		file.close()
-		print(thicc_arr)
-	else:
-		print("nothing to be done")
+	pass
+
+
+func _on_Button_pressed() -> void:
+	oh_no()
+
+
+func _on_Timer_timeout() -> void:
+	oh_no()
+
+
+func oh_no() -> void:
+	randomize()
+	for i in 9000000:
+#		print(i)
+		thicc_arr.append(1)
+#		thicc_arr.append(10)
+#		thicc_arr.append(randi())
+	file.open(path, File.WRITE)
+	file.store_var(thicc_arr)
+	file.close()
