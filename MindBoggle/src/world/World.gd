3d3cc3a05855e3c22544ffc684eb0e51b9819007
NAD LAB <61890796+aimforbigfoot@users.noreply.github.com>
2021-05-14 23:48:21 -0300

    info

--- MindBoggle/src/world/World.gd
+++ MindBoggle/src/world/World.gd
@@ -12,7 +12,8 @@ func switchToRoom2(body) -> void:
 #		$switcher.disconnect("body_entered",self,"switchToRoom2")
 		$switcher/CollisionShape.disabled = true
 		$shufllingObjects.play()
-
+# you can switch room 3 and 4 with room 1 and 2, just make sure to switch
+# room3 in the inspector to room4
 
 func _on_switcher_body_entered(body: Node) -> void:
 	pass # Replace with function body.
